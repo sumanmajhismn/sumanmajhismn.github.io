c865a5999412473d0162b60aba7105594df0b6c5
SMN76 <57235825+SMN76@users.noreply.github.com>
2020-11-27 22:34:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sumanmajhismn.github.io
---
+++ main.js
@@ -0,0 +1,10 @@
+// gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".projects",
+//         start: "140px 100px",
+//         end: "bottom top",
+//         pin: true,
+//         scrub: true
+//     }
+// })
+// .from(".projects .container .card-project", { x: innerHeight * 1  })
